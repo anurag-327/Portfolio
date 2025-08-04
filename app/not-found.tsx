01253d0ddc4e174963478bef496b140d21a27f3b
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2025-08-05 01:44:25 +0530

    fix: fixed build issues

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className="min-h-[88vh] flex items-center justify-center">
       <div className="text-center">
-        <h1 className="text-2xl mb-2">Bruh, you're in the void</h1>
+        <h1 className="text-2xl mb-2">Bruh, you&apos;re in the void</h1>
         <Link
           href="/"
           className="px-4 text-sm py-2 hover:underline text-zinc-800 dark:text-zinc-200 hover:opacity-70 "
